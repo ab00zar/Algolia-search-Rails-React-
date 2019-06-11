25f568075388bfbb8ff601c30a1d15ce2ffb9696
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2019-06-11 09:16:08 +0200

    TDD specs for show movies action

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -13,4 +13,20 @@ describe 'Movies API', type: :request do
             expect(response).to have_http_status(200)
         end
     end
+
+    describe 'GET /api/v1/movies/:id' do
+
+        context 'when the record exists' do
+            before { get '/api/v1/movies/439378210' }
+
+            it 'returns the movie' do
+                expect(json).not_to be_empty
+                expect(json['id']).to eq(439378210)
+            end
+
+            it 'return status code 200' do
+                expect(response).to have_http_status(200)
+            end
+        end
+    end
 end 
