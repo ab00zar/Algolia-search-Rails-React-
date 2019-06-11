4ad38939495ab163df8c56d35c48f3179202bf5c
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2019-06-12 01:52:10 +0200

    search method is added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-  # namespace 'api' do
-  #   namespace 'v1' do
-  #     resources :movies
-  #   end
-  # end
+
+  get :search, controller: :welcome
+
+  namespace 'api' do
+    namespace 'v1' do
+      resources :movies
+    end
+  end
+
 end
