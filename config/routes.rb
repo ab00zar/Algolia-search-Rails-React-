b12b0cad0b58471702a0fe1eecf354840fa420d6
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2019-06-11 22:52:14 +0200

    welcome controller & index view page added to handle root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
-      resources :movies
-    end
-  end
+  root to: 'welcome#index'
+  # namespace 'api' do
+  #   namespace 'v1' do
+  #     resources :movies
+  #   end
+  # end
 end
